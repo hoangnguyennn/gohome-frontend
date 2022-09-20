b2b55b9411ff5fc228f9c30d5b145a6abad21658
hoangnguyen <hoangnv@bap.jp>
2022-09-20 20:24:41 +0700

    setup layouts, pages, routing

---
+++ src/layouts/default.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined
+} from '@ant-design/icons-vue'
+import {
+  Layout,
+  LayoutSider,
+  LayoutHeader,
+  LayoutContent,
+  Menu,
+  MenuItem
+} from 'ant-design-vue'
+
+const collapsed = ref(false)
+const selectedKeys = ref(['1'])
+</script>
+
+<template>
+  <Layout id="layout-default">
+    <LayoutSider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <Menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <MenuItem key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </MenuItem>
+        <MenuItem key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </MenuItem>
+        <MenuItem key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </MenuItem>
+      </Menu>
+    </LayoutSider>
+    <Layout>
+      <LayoutHeader style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined
+          v-else
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </LayoutHeader>
+      <LayoutContent
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px'
+        }"
+      >
+        <RouterView />
+      </LayoutContent>
+    </Layout>
+  </Layout>
+</template>
+
+<style lang="scss" scoped>
+#layout-default {
+  height: 100vh;
+
+  .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #1890ff;
+    }
+  }
+
+  .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.3);
+    margin: 16px;
+  }
+
+  .site-layout-background {
+    background: #fff;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Pages(),
+    Layouts(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-imports.d.ts'
+    }),
+    Components({
+      dts: true
+    })
+  ]
 })
